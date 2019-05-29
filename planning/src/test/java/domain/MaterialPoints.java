b138a7c08d88e6202462ddb24e304cb3a259d609
kinnder <kinnder@mail.ru>
2019-05-30 01:08:09 +0300

    #4 use-case acceptance test проходит

--- planning/src/test/java/domain/MaterialPoints.java
+++ planning/src/test/java/domain/MaterialPoints.java
@@ -325,9 +325,9 @@ public class MaterialPoints {
 		systemVariants = initial_system.matchIds(moveBottom().getTemplate());
 		assertEquals(0, systemVariants.length);
 
-		System final_system = new System();
-		SystemObject final_object = (SystemObject) initial_object.clone();
-		SystemObject final_point_4 = (SystemObject) initial_point_4.clone();
+		final System final_system = new System();
+		final SystemObject final_object = (SystemObject) initial_object.clone();
+		final SystemObject final_point_4 = (SystemObject) initial_point_4.clone();
 		final_system.addObject(final_object);
 		final_system.addObject(final_point_4);
 
@@ -337,7 +337,7 @@ public class MaterialPoints {
 		final_system.getObjectById(initial_point_4_id).removeLink(LINK_NEIGHBOR_RIGHT);
 		final_system.getObjectById(initial_point_4_id).removeLink(LINK_NEIGHBOR_LEFT);
 
-		Element[] elements = new Element[] { moveLeft(), moveRight(), moveTop(), moveBottom() };
+		final Element[] elements = new Element[] { moveLeft(), moveRight(), moveTop(), moveBottom() };
 
 		Planner planner = new Planner(initial_system, final_system, elements);
 		planner.plan();
@@ -347,5 +347,257 @@ public class MaterialPoints {
 		assertEquals("Движение вправо", operations.get(0));
 		assertEquals("Движение вправо", operations.get(1));
 		assertEquals("Движение вправо", operations.get(2));
+	}
+
+	@Test
+	public void movingInTheField() {
+		// y
+		// 6 + - - - - - - - - - +
+		// 5 | . - - + - - - + . |
+		// 4 | A . . | . . . | . |
+		// 3 | . | . | . | . | . |
+		// 2 | . | . . . | . . B |
+		// 1 | . + - - - + - - . |
+		// 0 + - - - - - - - - - +
+		// 0 0 1 2 3 4 5 6 7 8 9 A x
+
+		final System initial_system = new System();
+		final SystemObject initial_object = new SystemObject("материальная точка");
+		final SystemObject initial_point_15 = new SystemObject("точка-15");
+		final SystemObject initial_point_14 = new SystemObject("точка-14");
+		final SystemObject initial_point_13 = new SystemObject("точка-13");
+		final SystemObject initial_point_12 = new SystemObject("точка-12");
+		final SystemObject initial_point_11 = new SystemObject("точка-11");
+		final SystemObject initial_point_24 = new SystemObject("точка-24");
+		final SystemObject initial_point_34 = new SystemObject("точка-34");
+		final SystemObject initial_point_33 = new SystemObject("точка-33");
+		final SystemObject initial_point_32 = new SystemObject("точка-32");
+		final SystemObject initial_point_42 = new SystemObject("точка-42");
+		final SystemObject initial_point_54 = new SystemObject("точка-54");
+		final SystemObject initial_point_53 = new SystemObject("точка-53");
+		final SystemObject initial_point_52 = new SystemObject("точка-52");
+		final SystemObject initial_point_64 = new SystemObject("точка-64");
+		final SystemObject initial_point_74 = new SystemObject("точка-74");
+		final SystemObject initial_point_73 = new SystemObject("точка-73");
+		final SystemObject initial_point_72 = new SystemObject("точка-72");
+		final SystemObject initial_point_82 = new SystemObject("точка-82");
+		final SystemObject initial_point_95 = new SystemObject("точка-95");
+		final SystemObject initial_point_94 = new SystemObject("точка-94");
+		final SystemObject initial_point_93 = new SystemObject("точка-93");
+		final SystemObject initial_point_92 = new SystemObject("точка-92");
+		final SystemObject initial_point_91 = new SystemObject("точка-91");
+
+		final String initial_object_id = initial_object.getObjectId();
+		final String initial_point_15_id = initial_point_15.getObjectId();
+		final String initial_point_14_id = initial_point_14.getObjectId();
+		final String initial_point_13_id = initial_point_13.getObjectId();
+		final String initial_point_12_id = initial_point_12.getObjectId();
+		final String initial_point_11_id = initial_point_11.getObjectId();
+		final String initial_point_24_id = initial_point_24.getObjectId();
+		final String initial_point_34_id = initial_point_34.getObjectId();
+		final String initial_point_33_id = initial_point_33.getObjectId();
+		final String initial_point_32_id = initial_point_32.getObjectId();
+		final String initial_point_42_id = initial_point_42.getObjectId();
+		final String initial_point_54_id = initial_point_54.getObjectId();
+		final String initial_point_53_id = initial_point_53.getObjectId();
+		final String initial_point_52_id = initial_point_52.getObjectId();
+		final String initial_point_64_id = initial_point_64.getObjectId();
+		final String initial_point_74_id = initial_point_74.getObjectId();
+		final String initial_point_73_id = initial_point_73.getObjectId();
+		final String initial_point_72_id = initial_point_72.getObjectId();
+		final String initial_point_82_id = initial_point_82.getObjectId();
+		final String initial_point_95_id = initial_point_95.getObjectId();
+		final String initial_point_94_id = initial_point_94.getObjectId();
+		final String initial_point_93_id = initial_point_93.getObjectId();
+		final String initial_point_92_id = initial_point_92.getObjectId();
+		final String initial_point_91_id = initial_point_91.getObjectId();
+
+		initial_object.addLink(new Link(LINK_POSITION, initial_point_14_id));
+
+		initial_point_15.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_15.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_14_id));
+		initial_point_15.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_14.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, true));
+		initial_point_14.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_15_id));
+		initial_point_14.addLink(new Link(LINK_NEIGHBOR_RIGHT, initial_point_24_id));
+		initial_point_14.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_13_id));
+		initial_point_14.addLink(new Link(LINK_POSITION, initial_object_id));
+
+		initial_point_13.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_13.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_14_id));
+		initial_point_13.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_12_id));
+		initial_point_13.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_12.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_12.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_13_id));
+		initial_point_12.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_11_id));
+		initial_point_12.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_11.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_11.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_12_id));
+		initial_point_11.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_24.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_24.addLink(new Link(LINK_NEIGHBOR_LEFT, initial_point_14_id));
+		initial_point_24.addLink(new Link(LINK_NEIGHBOR_RIGHT, initial_point_34_id));
+		initial_point_24.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_34.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_34.addLink(new Link(LINK_NEIGHBOR_LEFT, initial_point_24_id));
+		initial_point_34.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_33_id));
+		initial_point_34.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_33.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_33.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_34_id));
+		initial_point_33.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_32_id));
+		initial_point_33.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_32.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_32.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_33_id));
+		initial_point_32.addLink(new Link(LINK_NEIGHBOR_RIGHT, initial_point_42_id));
+		initial_point_32.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_42.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_42.addLink(new Link(LINK_NEIGHBOR_LEFT, initial_point_32_id));
+		initial_point_42.addLink(new Link(LINK_NEIGHBOR_RIGHT, initial_point_52_id));
+		initial_point_42.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_52.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_52.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_53_id));
+		initial_point_52.addLink(new Link(LINK_NEIGHBOR_LEFT, initial_point_42_id));
+		initial_point_52.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_53.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_53.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_54_id));
+		initial_point_53.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_52_id));
+		initial_point_53.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_54.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_54.addLink(new Link(LINK_NEIGHBOR_RIGHT, initial_point_64_id));
+		initial_point_54.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_53_id));
+		initial_point_54.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_64.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_64.addLink(new Link(LINK_NEIGHBOR_LEFT, initial_point_54_id));
+		initial_point_64.addLink(new Link(LINK_NEIGHBOR_RIGHT, initial_point_74_id));
+		initial_point_64.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_74.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_74.addLink(new Link(LINK_NEIGHBOR_LEFT, initial_point_64_id));
+		initial_point_74.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_73_id));
+		initial_point_74.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_73.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_73.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_74_id));
+		initial_point_73.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_72_id));
+		initial_point_73.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_72.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_72.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_73_id));
+		initial_point_72.addLink(new Link(LINK_NEIGHBOR_RIGHT, initial_point_82_id));
+		initial_point_72.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_82.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_82.addLink(new Link(LINK_NEIGHBOR_LEFT, initial_point_72_id));
+		initial_point_82.addLink(new Link(LINK_NEIGHBOR_RIGHT, initial_point_92_id));
+		initial_point_82.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_95.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_95.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_94_id));
+		initial_point_95.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_94.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_94.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_95_id));
+		initial_point_94.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_93_id));
+		initial_point_94.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_93.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_93.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_94_id));
+		initial_point_93.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_92_id));
+		initial_point_93.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_92.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_92.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_93_id));
+		initial_point_92.addLink(new Link(LINK_NEIGHBOR_LEFT, initial_point_82_id));
+		initial_point_92.addLink(new Link(LINK_NEIGHBOR_BOTTOM, initial_point_91_id));
+		initial_point_92.addLink(new Link(LINK_POSITION, null));
+
+		initial_point_91.addAttribute(new Attribute(ATTRIBUTE_OCCUPIED, false));
+		initial_point_91.addLink(new Link(LINK_NEIGHBOR_TOP, initial_point_92_id));
+		initial_point_91.addLink(new Link(LINK_POSITION, null));
+
+		initial_system.addObject(initial_object);
+		initial_system.addObject(initial_point_15);
+		initial_system.addObject(initial_point_14);
+		initial_system.addObject(initial_point_13);
+		initial_system.addObject(initial_point_12);
+		initial_system.addObject(initial_point_11);
+		initial_system.addObject(initial_point_24);
+		initial_system.addObject(initial_point_34);
+		initial_system.addObject(initial_point_33);
+		initial_system.addObject(initial_point_32);
+		initial_system.addObject(initial_point_42);
+		initial_system.addObject(initial_point_54);
+		initial_system.addObject(initial_point_53);
+		initial_system.addObject(initial_point_52);
+		initial_system.addObject(initial_point_64);
+		initial_system.addObject(initial_point_74);
+		initial_system.addObject(initial_point_73);
+		initial_system.addObject(initial_point_72);
+		initial_system.addObject(initial_point_82);
+		initial_system.addObject(initial_point_95);
+		initial_system.addObject(initial_point_94);
+		initial_system.addObject(initial_point_93);
+		initial_system.addObject(initial_point_92);
+		initial_system.addObject(initial_point_91);
+
+		SystemVariant[] systemVariants;
+		systemVariants = initial_system.matchIds(moveRight().getTemplate());
+		assertEquals(1, systemVariants.length);
+
+		systemVariants = initial_system.matchIds(moveLeft().getTemplate());
+		assertEquals(0, systemVariants.length);
+
+		systemVariants = initial_system.matchIds(moveTop().getTemplate());
+		assertEquals(1, systemVariants.length);
+
+		systemVariants = initial_system.matchIds(moveBottom().getTemplate());
+		assertEquals(1, systemVariants.length);
+
+		final System final_system = new System();
+		final SystemObject final_object = (SystemObject) initial_object.clone();
+		final SystemObject final_point_92 = (SystemObject) initial_point_92.clone();
+		final_system.addObject(final_object);
+		final_system.addObject(final_point_92);
+
+		final_system.getObjectById(initial_object_id).getLink(LINK_POSITION).setObjectId(initial_point_92_id);
+		final_system.getObjectById(initial_point_92_id).getAttribute(ATTRIBUTE_OCCUPIED).setValue(true);
+		final_system.getObjectById(initial_point_92_id).getLink(LINK_POSITION).setObjectId(initial_object_id);
+		final_system.getObjectById(initial_point_92_id).removeLink(LINK_NEIGHBOR_TOP);
+		final_system.getObjectById(initial_point_92_id).removeLink(LINK_NEIGHBOR_LEFT);
+		final_system.getObjectById(initial_point_92_id).removeLink(LINK_NEIGHBOR_BOTTOM);
+
+		final Element[] elements = new Element[] { moveLeft(), moveRight(), moveTop(), moveBottom() };
+
+		Planner planner = new Planner(initial_system, final_system, elements);
+		planner.plan();
+
+		List<String> operations = planner.getShortestPlan();
+		assertEquals(14, operations.size());
+		assertEquals("Движение вправо", operations.get(0));
+		assertEquals("Движение вправо", operations.get(1));
+		assertEquals("Движение вниз", operations.get(2));
+		assertEquals("Движение вниз", operations.get(3));
+		assertEquals("Движение вправо", operations.get(4));
+		assertEquals("Движение вправо", operations.get(5));
+		assertEquals("Движение вверх", operations.get(6));
+		assertEquals("Движение вверх", operations.get(7));
+		assertEquals("Движение вправо", operations.get(8));
+		assertEquals("Движение вправо", operations.get(9));
+		assertEquals("Движение вниз", operations.get(10));
+		assertEquals("Движение вниз", operations.get(11));
+		assertEquals("Движение вправо", operations.get(12));
+		assertEquals("Движение вправо", operations.get(13));
 	}
 }
