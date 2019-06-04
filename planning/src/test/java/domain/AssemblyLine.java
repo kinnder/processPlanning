e146a2ff455eed68ad17e9d9503363f85b3d1c00
kinnder <kinnder@mail.ru>
2019-06-04 23:12:27 +0300

    #6 типовой элемент "повернуть робот к транспортной линии"

---
+++ planning/src/test/java/domain/AssemblyLine.java
@@ -0,0 +1,62 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import planning.model.Attribute;
+import planning.model.Element;
+import planning.model.Link;
+import planning.model.LinkTransformation;
+import planning.model.System;
+import planning.model.SystemObject;
+import planning.model.Transformation;
+
+public class AssemblyLine {
+
+	private static final String OBJECT_PICK_AND_PLACE_ROBOT = "Робот-перекладчик";
+
+	private static final String OBJECT_CONFIGURATION_STATION = "Станция комплектации";
+
+	private static final String OBJECT_TRANSPORT_LINE = "Транспортная линия";
+
+	private static final String ATTRIBUTE_GRAB = "захват";
+
+	private static final String LINK_BELONGS = "принадлежит";
+
+	private static final String LINK_ROTATED = "повернут";
+
+	public static Element rotateRobotToTransportLine() {
+		System template = new System();
+		SystemObject robot = new SystemObject(OBJECT_PICK_AND_PLACE_ROBOT, "#ID-1");
+		SystemObject station = new SystemObject(OBJECT_CONFIGURATION_STATION, "#ID-2");
+		SystemObject line = new SystemObject(OBJECT_TRANSPORT_LINE, "#ID-3");
+
+		final String robot_id = robot.getObjectId();
+		final String station_id = station.getObjectId();
+		final String transportLine_id = line.getObjectId();
+
+		robot.addAttribute(new Attribute(ATTRIBUTE_GRAB, "открыт"));
+		robot.addLink(new Link(LINK_BELONGS, station_id));
+		robot.addLink(new Link(LINK_ROTATED, station_id));
+
+		station.addLink(new Link(LINK_BELONGS, transportLine_id));
+		station.addLink(new Link(LINK_ROTATED, robot_id));
+
+		line.addLink(new Link(LINK_ROTATED, null));
+
+		template.addObject(robot);
+		template.addObject(station);
+		template.addObject(line);
+
+		Transformation transformations[] = new Transformation[] {
+				new LinkTransformation(robot_id, LINK_ROTATED, transportLine_id),
+				new LinkTransformation(station_id, LINK_ROTATED, null),
+				new LinkTransformation(transportLine_id, LINK_ROTATED, robot_id) };
+
+		return new Element("Повернуть робот-перекладчик к транспортной линии", template, transformations);
+	}
+
+	@Test
+	public void applicationOfElements() {
+		rotateRobotToTransportLine();
+	}
+}
