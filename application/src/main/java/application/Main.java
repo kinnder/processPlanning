0ee4957983b62642104e567d35a81d3133f70cc4
kinnder <kinnder@mail.ru>
2020-03-11 15:08:39 +0300

    #20 вызов планировщика с параметрами командной строки

--- application/src/main/java/application/Main.java
+++ application/src/main/java/application/Main.java
@@ -1,11 +1,26 @@
 package application;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.jdom2.JDOMException;
+
+import planning.method.Planner;
+import planning.method.SystemTransformations;
+import planning.method.TaskDescription;
+import planning.model.SystemProcess;
+import planning.storage.SystemProcessXMLFile;
+import planning.storage.SystemTransformationsXMLFile;
+import planning.storage.TaskDescriptionXMLFile;
 
 // TODO : remove this annotation
 @SuppressWarnings("PMD")
@@ -14,42 +29,66 @@ public class Main {
 	public static void main(String[] args) {
 		CommandLineParser parser = new DefaultParser();
 
-		Option help = new Option("h", "help", false, "prints usage");
+		Option helpOption = new Option("h", "help", false, "prints usage");
 
-		Option taskDescription = new Option("td", "taskDescription", true, "file with description of the task");
-		Option systemTransformations = new Option("st", "systemTransformations", true,
+		Option taskDescriptionOption = new Option("td", "taskDescription", true, "file with description of the task");
+		Option systemTransformationsOption = new Option("st", "systemTransformations", true,
 				"file with description of the system transformations");
-		Option process = new Option("p", "process", true, "output file");
+		Option processOption = new Option("p", "process", true, "output file");
 
 		Options options = new Options();
-		options.addOption(help);
-		options.addOption(taskDescription);
-		options.addOption(systemTransformations);
-		options.addOption(process);
+		options.addOption(helpOption);
+		options.addOption(taskDescriptionOption);
+		options.addOption(systemTransformationsOption);
+		options.addOption(processOption);
 
-		args = new String[] { "-help" };
+		// TODO : remove
+//		args = new String[] { "-help" };
 
 		try {
 			CommandLine line = parser.parse(options, args);
 
-			String taskDescriptionFile = line.getOptionValue(taskDescription.getOpt(), "taskDescription.xml");
-			String systemTransformationsFile = line.getOptionValue(systemTransformations.getOpt(),
+			String taskDescriptionFile = line.getOptionValue(taskDescriptionOption.getOpt(), "taskDescription.xml");
+			String systemTransformationsFile = line.getOptionValue(systemTransformationsOption.getOpt(),
 					"systemTransformations.xml");
-			String processFile = line.getOptionValue(process.getOpt(), "process.xml");
+			String processFile = line.getOptionValue(processOption.getOpt(), "process.xml");
 
-			if (line.hasOption(help.getOpt())) {
-				System.out.println(
+			if (line.hasOption(helpOption.getOpt())) {
+				// TODO : resolve conflict with base class
+				java.lang.System.out.println(
 						"application builds plan for [taskDescription] with [systemTransformations] and puts result in [process]");
-				System.out.println("usage:");
+				java.lang.System.out.println("usage:");
 				for (Option option : options.getOptions()) {
 					StringBuilder sb = new StringBuilder();
 					sb.append(String.format("%2s, %-21s", option.getOpt(), option.getLongOpt()));
 					sb.append(" ");
 					sb.append(option.getDescription());
-					System.out.println(sb.toString());
+					java.lang.System.out.println(sb.toString());
 				}
 			} else {
-				// TODO : call planning
+				java.lang.System.out.println("planning...");
+				try {
+					SystemTransformationsXMLFile transformationsXMLFile = new SystemTransformationsXMLFile();
+					transformationsXMLFile.load(new BufferedInputStream(Files.newInputStream(Paths.get(systemTransformationsFile))));
+					SystemTransformations systemTransformations = new SystemTransformations();
+					systemTransformations.addAll(transformationsXMLFile.getObject());
+
+					TaskDescriptionXMLFile taskXMLFile = new TaskDescriptionXMLFile();
+					taskXMLFile.load(new BufferedInputStream(Files.newInputStream(Paths.get(taskDescriptionFile))));
+					TaskDescription taskDescription = taskXMLFile.getObject();
+
+					Planner planner = new Planner(taskDescription, systemTransformations);
+					planner.plan();
+
+					SystemProcess operations = planner.getShortestProcess();
+					SystemProcessXMLFile xmlFile = new SystemProcessXMLFile();
+					xmlFile.setObject(operations);
+					xmlFile.save(new BufferedOutputStream(Files.newOutputStream(Paths.get(processFile))));
+				} catch (JDOMException | CloneNotSupportedException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				java.lang.System.out.println("done");
 			}
 
 		} catch (ParseException e) {
