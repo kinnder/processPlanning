ac5bd6ac78bb8c926815377656ef5bccba959509
kinnder <kinnder@mail.ru>
2021-06-24 14:11:52 +0300

    #35 содержимое пакетных файлов перенесено в примеры использования

--- tests/assemblyLine_owl.bat
+++
@@ -1,5 +0,0 @@
-java -jar .\application-0.0.1.jar -new_td assemblyLine -td "assemblyLine_td.owl"
-
-java -jar .\application-0.0.1.jar -new_st assemblyLine -st "assemblyLine_st.owl"
-
-java -jar .\application-0.0.1.jar -plan -td "assemblyLine_td.owl" -st "assemblyLine_st.owl" -nn "assemblyLine_nn.owl" -p "assemblyLine_p.owl"
--- tests/materialPoints_owl.bat
+++
@@ -1,5 +0,0 @@
-java -jar .\application-0.0.1.jar -new_td materialPoints -td "materialPoints_td.owl"
-
-java -jar .\application-0.0.1.jar -new_st materialPoints -st "materialPoints_st.owl"
-
-java -jar .\application-0.0.1.jar -plan -td "materialPoints_td.owl" -st "materialPoints_st.owl" -nn "materialPoints_nn.owl" -p "materialPoints_p.owl"
