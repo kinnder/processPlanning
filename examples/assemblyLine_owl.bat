52ac88c920c2e9489d8ede3a0977791a0283ab32
kinnder <kinnder@mail.ru>
2022-12-30 18:01:13 +0300

    #68 обновлены examples

--- examples/assemblyLine_owl.bat
+++ examples/assemblyLine_owl.bat
@@ -1,5 +1,5 @@
-java -jar .\application-0.2.0.jar -new_td -d "assemblyLine" -td "assemblyLine_td.owl"
+java -jar .\application-0.3.0.jar -new_td -d "assemblyLine" -td "assemblyLine_td.owl"
 
-java -jar .\application-0.2.0.jar -new_st -d "assemblyLine" -st "assemblyLine_st.owl"
+java -jar .\application-0.3.0.jar -new_st -d "assemblyLine" -st "assemblyLine_st.owl"
 
-java -jar .\application-0.2.0.jar -plan -td "assemblyLine_td.owl" -st "assemblyLine_st.owl" -nn "assemblyLine_nn.owl" -p "assemblyLine_p.owl"
+java -jar .\application-0.3.0.jar -plan -td "assemblyLine_td.owl" -st "assemblyLine_st.owl" -nn "assemblyLine_nn.owl" -p "assemblyLine_p.owl"
--- examples/materialPoints_owl.bat
+++ examples/materialPoints_owl.bat
@@ -1,5 +1,5 @@
-java -jar .\application-0.2.0.jar -new_td -d "materialPoints" -td "materialPoints_td.owl"
+java -jar .\application-0.3.0.jar -new_td -d "materialPoints" -td "materialPoints_td.owl"
 
-java -jar .\application-0.2.0.jar -new_st -d "materialPoints" -st "materialPoints_st.owl"
+java -jar .\application-0.3.0.jar -new_st -d "materialPoints" -st "materialPoints_st.owl"
 
-java -jar .\application-0.2.0.jar -plan -td "materialPoints_td.owl" -st "materialPoints_st.owl" -nn "materialPoints_nn.owl" -p "materialPoints_p.owl"
+java -jar .\application-0.3.0.jar -plan -td "materialPoints_td.owl" -st "materialPoints_st.owl" -nn "materialPoints_nn.owl" -p "materialPoints_p.owl"
